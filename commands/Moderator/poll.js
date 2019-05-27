4e333bea5f95632dc68034e0fc91640e20e71ada
Jaecko <jackson-jamais203@hotmail.com>
2019-05-27 20:41:46 +0200

    Procfile

--- commands/Moderator/poll.js
+++ commands/Moderator/poll.js
@@ -1,33 +1,33 @@
 /* eslint-disable */
 
-const Command = require('../../modules/Command.js');
-const Discord = require('discord.js');
+const Command = require("../../modules/Command.js");
+const Discord = require("discord.js");
 
 class Poll extends Command {
   constructor(client) {
     super(client, {
-      name: 'poll',
-      description: 'Créer un sondage.',
-      usage: 'poll <question>',
-      category: 'Staff',
-      permLevel: 'Staff'
+      name: "poll",
+      description: "Créer un sondage.",
+      usage: "poll <question>",
+      category: "Staff",
+      permLevel: "Staff"
     });
   }
 
   async run(message, args) {
     try {
-      if (!args[0]) return message.channel.send('Syntaxe: poll <question>');
+      if (!args[0]) return message.channel.send("Syntaxe: poll <question>");
 
       const pollEmbed = new Discord.RichEmbed()
         .setTitle(`__Sondage créé par ${message.author.username}__`)
-        .setColor('#dc143c')
-        .setFooter('Appuyez que les réactions ci-dessous.')
-        .setDescription(args.join(' '));
+        .setColor("#dc143c")
+        .setFooter("Appuyez que les réactions ci-dessous.")
+        .setDescription(args.join(" "));
 
       message.delete(2000);
       const msg = await message.channel.send(pollEmbed);
-      await msg.react('✔');
-      await msg.react('❌');
+      await msg.react("✔");
+      await msg.react("❌");
     } catch (e) {
       console.log(e);
     }
